7e6079020a0ba8092cb8334f3dae7b26a5a3dc55
Ron G <ronald.greeff@gmail.com>
2020-09-29 11:57:31 +0100

    Basic Types

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 Getting Started With GraphQL.js
 https://graphql.org/graphql-js/
+
+To handle GraphQl queries, define SCHEMA that defines the Query type - note that use of back quotes.
+
+We also need an API root with a RESOLVER function per API endpoint
+
+Basic api server:
+  `var app = express();
+   app.use('/graphql', graphqlHTTP({
+     schema: schema,
+     rootValue: root,
+     graphiql: true,
+   }));
+   app.listen(4000);`
+
+graphical: true enables GraphiQL interactive interface
+
+
+When passing in arguments via browser, use $var syntax as below
+Example schema:
+ `type Query {
+    rollDice(numDice: Int!, numSides: Int): [Int]
+  }`
+
+Browser code to use:
+var dice = 3;
+var sides = 6;
+var query = `query RollDice($dice: Int!, $sides: Int) {
+ rollDice(numDice: $dice, numSides: $sides)
+}`;
+
+fetch('/graphql', {
+ method: 'POST',
+ headers: {
+   'Content-Type': 'application/json',
+   'Accept': 'application/json',
+ },
+ body: JSON.stringify({
+   query, //                        <--- auto-escapes $dice and
+   variables: { dice, sides }, //   <--- $sides vars in query
+ })
+})
+ .then(r => r.json())
+ .then(data => console.log('data returned:', data));
--- server.js
+++ server.js
@@ -2,23 +2,31 @@ var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
 
-// To handle GraphQl queries, define SCHEMA that defines
-// the Query type - note that use of back quotes: (`` not '')
+// GraphQL schema language supports the scalar types of String, Int,
+// Float, Boolean, and ID, so you can use these directly in the schema
+// By default every type is nullable
+// Use an exclamation point to indicate a type non-nullable, e.g. String!
+
 var schema = buildSchema(`
   type Query {
-    hello: String
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
   }
 `);
 
-// We also need an API root with a RESOLVER function
-// per API endpoint
 var root = {
-  hello: () => {
-    return 'Hello world';
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
   },
+  random: () => {
+    return Math.random();
+  },
+  rollThreeDice: () => {
+    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
+  }
 };
 
-// API Server. graphiql: true enables GraphiQL interactive interface
 var app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
@@ -26,57 +34,5 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true,
 }));
 app.listen(4000);
+
 console.log('Running a GraphQL API server at http://localhost:4000/graphql');
-
-// querying { hello } will return you { data: { hello: 'Hello world!' } }
-
-
-// GraphQL Clients
-// *Relay is a powerful client that handles batching, caching and more*
-
-// HTTP POST request to endpoint with query field in JSON payload
-// curl -X POST
-// -H "Content-Type: application/json"
-// -d '{"query": "{ hello }"}' http://localhost:4000/graphql
-
-// in Brower @ localhost:4000/graphql
-// fetch('/graphql', {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json',
-//     'Accept': 'application/json',
-//   },
-//   body: JSON.stringify({query: "{ hello }"})
-// })
-//   .then(r => r.json())
-//   .then(data => console.log('data returned:', data));
-
-
-
-// When passing in arguments via browser, use $var syntax as below
-
-// eg. schema
-// `type Query {
-//   rollDice(numDice: Int!, numSides: Int): [Int]
-// }`
-//
-// Browser
-// var dice = 3;
-// var sides = 6;
-// var query = `query RollDice($dice: Int!, $sides: Int) {
-//   rollDice(numDice: $dice, numSides: $sides)
-// }`;
-//
-// fetch('/graphql', {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json',
-//     'Accept': 'application/json',
-//   },
-//   body: JSON.stringify({
-//     query,                       <--- autoescapes vars
-//     variables: { dice, sides },  <--- $dice and $sides
-//   })
-// })
-//   .then(r => r.json())
-//   .then(data => console.log('data returned:', data));

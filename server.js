be570ab0777eed7440c715a2df7998d4253f8adb
Ron G <ronald.greeff@gmail.com>
2020-09-29 10:55:29 +0100

    Running an Express GraphQL Server

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var { graphql, buildSchema } = require('graphql');
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
 
 // To handle GraphQl queries, define SCHEMA that defines
 // the Query type - note that use of back quotes: (`` not '')
@@ -16,6 +18,14 @@ var root = {
   },
 };
 
-graphql(schema, '{ hello }', root).then((response) => {
-  console.log(response);
-});
+// API Server. graphiql: true enables GraphiQL interactive interface
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+
+// { hello } will return you { data: { hello: 'Hello world!' } }

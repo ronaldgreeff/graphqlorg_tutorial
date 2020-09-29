751819fd1951eb0193629a36540c551c570dc1cd
Ron G <ronald.greeff@gmail.com>
2020-09-29 12:49:04 +0100

    Object Types

--- server.js
+++ server.js
@@ -2,41 +2,53 @@ var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
 
-// GraphQL schema language supports the scalar types of String, Int,
-// Float, Boolean, and ID, so you can use these directly in the schema
-// By default every type is nullable
-// Use an exclamation point to indicate a type non-nullable, e.g. String!
-
-// method(params and type): type returned
-// rollDice(numDice: Int!, numSides: Int): [Int]
-
+// Most times, you want to return an object from the API, not just a val
+// from this:
+  // type Query {
+  //   rollDice(numDice: Int!, numSides: Int): [Int]
+  // }
+// to this RandomDie object:
 var schema = buildSchema(`
+  type RandomDie {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
+  }
   type Query {
-    quoteOfTheDay: String
-    random: Float!
-    rollThreeDice: [Int]
-    rollDice(numDice: Int!, numSides: Int): [Int]
+    getDie(numSides: Int): RandomDie
   }
 `);
 
-var root = {
-  quoteOfTheDay: () => {
-    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
-  },
-  random: () => {
-    return Math.random();
-  },
-  rollThreeDice: () => {
-    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
-  },
-  rollDice: ({numDice, numSides}) => {
+// Instead of root level resolver, can define a class where resolvers
+// are methods of the instance - This ES6 class implements the
+// RandomDie GraphQL type:
+class RandomDie {
+  constructor(numSides) {
+    this.numSides = numSides;
+  }
+  rollOnce() {
+    return 1 + Math.floor(Math.random() * this.numSides);
+  }
+  roll({numRolls}) {
     var output = [];
-    for (var i=0; i < numDice; i++) {
-      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    for (var i=0; i<numRolls; i++) {
+      output.push(this.rollOnce());
     }
     return output;
   }
+}
+
+var root = {
+  getDie: ({numSides}) => {
+    return new RandomDie(numSides || 6);
+  }
 };
+// then use the query (see notes at end):
+// {
+//   getDie(numSides: 6) {
+//     rollOnce
+//   }
+// }
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
@@ -46,3 +58,21 @@ app.use('/graphql', graphqlHTTP({
 }));
 app.listen(4000);
 console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+
+// Notes:
+// if you run:
+//
+// {
+//   getDie(numSides: 6)
+// }
+//
+// You get the following error message:
+//   "Field \"getDie\" of type \"RandomDie\" must have a selection of subfields. Did you mean \"getDie { ... }\"?",
+//
+// You need:
+//
+// {
+//   getDie(numSides: 6) {
+//     roll(numRolls: 3)
+//   }
+// }
